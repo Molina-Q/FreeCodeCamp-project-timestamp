0c6a20755892ee693464d593897757e6b5fd9992
Molina-Q <quentinmolina1@gmail.com>
2024-02-26 13:48:41 +0100

    Change to get method

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
- res.json({greeting: 'hello API'});
+  res.json({greeting: 'hello API'});
 });
 
 /***** route structure *****/ 
@@ -33,33 +33,58 @@ app.get("/api/hello", function (req, res) {
 app.route('/api/:date?')
   .get(function (req, res, next) {
     givenDate = req.params.date;
-    if (typeof givenDate  === "undefined") { // case where the given date is empty
-      error = new Date(givenDate).toString();
+    errorEmpty = false;
+    givenDateInt = parseInt(req.params.date);
 
+    if(!req.params.date) {
+      //if(typeof givenDate  === "undefined") {
+      errorEmpty = true;
+    }
 
-      objectSent = {error};
- 
- 
+    if (errorEmpty) { // case where the given date is empty
+      dateNow = new Date();
 
-    } else if (givenDate.includes('-')) { // case where the given date is 2015-12-12
-      unixString = Date.parse(givenDate);
+      unixString = Date.parse(dateNow);
       unixTime = parseInt(unixString);
-      utcTime = new Date(givenDate).toUTCString();
+
+      utcTime = new Date(dateNow).toUTCString();
+
+      objectSent = {unix: unixTime, utc: utcTime};
+
+    } 
+    
+    else if (new Date(parseInt(givenDate)).toString() === "Invalid Date" ) { // case where the given date is empty
+      objectSent = {error: "Invalid Date"};
+    } 
+
+    else if (givenDate.includes('-')) { // case where the given date is 2015-12-12
+      objDate = new Date(givenDate);
+
+      unixString = Date.parse(objDate);
+      unixTime = parseInt(unixString);
+
+      utcTime = objDate.toUTCString();
+
       objectSent = {unix: unixTime, utc: utcTime};
 
 
     } else { // case where the given date is 1616608200
+
       unixTime = parseInt(givenDate);
-      utcTime = new Date(unixTime).toUTCString();
+
+      objDate = new Date(unixTime);
+
+      utcTime = objDate.toUTCString();
+
       objectSent = {unix: unixTime, utc: utcTime};
 
     }
-
+    console.log( new Date(parseInt(givenDate)).toString() );
+    
     next();
   }, function(req, res) {
     res.send(objectSent);
   })
-
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
